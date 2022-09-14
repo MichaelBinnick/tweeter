3880f1a3199f7b4ef9b9260ffa390852d5316f93
Michael Binnick <mbinnick@live.ca>
2022-09-14 22:37:40 +0000

    pulls tweets from /tweets/ and implemented alerts detecting invalid new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const { render } = require("express/lib/response");
-
 const createTweetElement = function(tweetObject) {
   const element = $(`
     <article class="tweet">
@@ -18,7 +16,7 @@ const createTweetElement = function(tweetObject) {
       </header>
       <p>${tweetObject.content.text}</p>
       <footer>
-        <p>${tweetObject.created_at}</p>
+        <p>${timeago.format(tweetObject.created_at)}</p>
         <div>
           <i class="fas fa-flag fa-xs"></i>
           <i class="fas fa-retweet fa-xs"></i>
@@ -41,30 +39,25 @@ const renderTweets = function(tweets) {
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  const loadTweets = function() {
+    $.ajax('/tweets/', { method: 'GET' })
+    .then(function (tweetsData) {
+      renderTweets(tweetsData);
+    });
+  };
 
-  renderTweets(data);
+  loadTweets();
+
+
+  $('.new-tweet-form').submit(function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    if (serializedData.length <= 5) {
+      return alert('The tweet is empty!');
+    }
+    if (serializedData.length > 140) {
+      return alert("You've entered too many characters! Please reduce your tweet.");
+    }
+    $.post('/tweets/', serializedData)
+  })
 });
